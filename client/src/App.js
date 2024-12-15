4a4833202ac09d3d8a412e3db27a27e99df7cc2c
Kostiantyn Dubovtsev <dubovtsevko@gmail.com>
2024-12-15 18:22:52 +0200

    client app is now not a submodule

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import GoogleAuth from './components/googleAuth.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HELLO</h1>
+      <GoogleAuth />
+    </div>
+  );
+}
+
+export default App;
